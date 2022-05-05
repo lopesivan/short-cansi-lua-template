bcc3725d1397f005bbb19a89702f2ac117cfa1d3
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-05-05 04:12:38 -0300

    Atualiza o arquivo src/main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
+// loadmodule.c
+//
+// Loads mymodule.lua but doesn't call any functions in it.
+//
 
-// $ cc doscript.c -o doscript -llua -L../lua -I../lua -lm -ldl
 #include "lauxlib.h"
 #include "lua.h"
 #include "lualib.h"
 
-int main()
-{
-    lua_State *L = luaL_newstate();
-    luaL_openlibs (L);
-    luaL_dofile (L, "script.lua");
+int main() {
+  lua_State *L = luaL_newstate();
+  luaL_openlibs(L);
+  luaL_dofile(L, "mymodule.lua");
+  lua_setglobal(L, "mymodule");
+  lua_settop(L, 0);
 
-    return 0;
+  return 0;
 }
